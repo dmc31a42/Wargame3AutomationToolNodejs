e33ba3bf027ba7ec48dae8aba73359450f561e3d
최낙원 <ch0494@gmail.com>
2018-01-31 00:28:31 +0900

    angularjs + socketio

--- control.js
+++ control.js
@@ -2,7 +2,80 @@
 var players = {};
 var infoRun = true;
 
+var app = require('express')();
+var express = require('express');
+var server = require('http').createServer(app);
+// http server를 socket.io server로 upgrade한다
+var io = require('socket.io')(server);
 
+// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.use(express.static('public'));
+//app.get('/', function(req, res) {
+//  res.sendFile(__dirname + '/index.html');
+//});
+
+// connection event handler
+// connection이 수립되면 event handler function의 인자로 socket인 들어온다
+io.on('connection', function(socket) {
+
+  // 접속한 클라이언트의 정보가 수신되면
+  socket.on('login', function(data) {
+    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+
+    // socket에 클라이언트 정보를 저장한다
+    socket.name = data.name;
+    socket.userid = data.userid;
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    io.emit('login', data.name );
+
+    setInterval(function(){
+      var msg = {
+          ServerSettings: ServerSettings,
+          players: players
+            };
+      io.emit('chat',msg);
+        },1000);
+  });
+
+  // 클라이언트로부터의 메시지가 수신되면
+  socket.on('chat', function(data) {
+    console.log('Message from %s: %s', socket.name, data.msg);
+
+    var msg = {
+      from: {
+        name: socket.name,
+        userid: socket.userid
+      },
+      msg: data.msg
+    };
+
+    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+    //socket.broadcast.emit('chat', msg);
+
+    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+    // socket.emit('s2c chat', msg);
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    io.emit('chat', msg);
+
+    // 특정 클라이언트에게만 메시지를 전송한다
+    // io.to(id).emit('s2c chat', data);
+  });
+
+  // force client disconnect from server
+  socket.on('forceDisconnect', function() {
+    socket.disconnect();
+  })
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected: ' + socket.name);
+  });
+});
+
+server.listen(3000, function() {
+  console.log('Socket IO server listening on port 3000');
+});
 
 Tail = require('tail').Tail;
 
@@ -57,7 +130,7 @@ function register_events(){
   register_event('Launch game', _on_switch_to_launch);
   register_event('Canceling launch game', _on_switch_to_cancel_launch);
 /* Server Status */
-  register_event('Variable (.*) set to (.*)', setServerSetting);
+  register_event('Variable (.*) set to "*([^"]*)', setServerSetting);
 }
 
 function setServerSetting(RegExpExec){
---
+++ public/app/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var app = angular.module('Wargame3AutomationTool', [
+]);
+
+app.run(['socket','$rootScope',
+  function(socket, $rootScope){
+    
+    socket.emit('login', {
+      name: "test",
+      userid: "test@test.com"
+    });
+
+    socket.on('chat', function(data){
+      $rootScope.ServerSettings = data.ServerSettings;
+      $rootScope.players = data.players;
+    });
+}]);
+
+
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
